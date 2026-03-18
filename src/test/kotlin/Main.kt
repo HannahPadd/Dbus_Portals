53187b9266c532dbc0ff201ea31e871e95b497b6
HannahPadd <hannah@lindrob.nl>
2026-03-18 16:05:51 +0100

    Update test

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -16,7 +16,12 @@ class LibraryTest {
     fun testCreateShortcut() {
         var isRunning = true
         val portalManager = PortalManager(APP_ID)
-        val shortcutsList = mutableListOf(ShortcutTuple("YAW_RESET_5", mapOf("description" to Variant("Yaw Reset"))))
+        val shortcutsList = mutableListOf(
+            ShortcutTuple("FULL_RESET", mapOf("description" to Variant("Full Reset"), "trigger_description" to Variant("CTRL+ALT+SHIFT+Y"))),
+            ShortcutTuple("YAW_RESET", mapOf("description" to Variant("Yaw Reset"), "trigger_description" to Variant("CTRL+ALT+SHIFT+U"))),
+            ShortcutTuple("MOUNTING_RESET", mapOf("description" to Variant("Mounting Reset"), "trigger_description" to Variant("CTRL+ALT+SHIFT+I"))),
+            ShortcutTuple("FEET_MOUNTING_RESET", mapOf("description" to Variant("Feet Mounting Reset"), "trigger_description" to Variant("CTRL+ALT+SHIFT+P"))),
+            ShortcutTuple("PAUSE_TRACKING", mapOf("description" to Variant("Pause Tracking"), "trigger_description" to Variant("CTRL+ALT+SHIFT+O"))))
         val globalShortcutsHandler = portalManager.globalShortcutsRequest(shortcutsList)
         Runtime.getRuntime().addShutdownHook(Thread {
             println("Closing connection")
@@ -26,8 +31,20 @@ class LibraryTest {
          while (isRunning) {
             globalShortcutsHandler.onShortcutActivated = { shortcutId ->
                 when (shortcutId) {
-                    "YAW_RESET_5" -> {
+                    "FULL_RESET" -> {
+                        println("Full reset triggered")
+                    }
+                    "YAW_RESET" -> {
                         println("Yaw reset triggered")
+                    }
+                    "MOUNTING_RESET" -> {
+                        println("Mounting reset triggered")
+                    }
+                    "FEET_MOUNTING_RESET" -> {
+                        println("Feet mounting reset triggered")
+                    }
+                    "PAUSE_TRACKING" -> {
+                        println("Pause tracking triggered")
                         isRunning = false
                     }
                 }
